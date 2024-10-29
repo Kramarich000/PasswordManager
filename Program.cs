f1890f2d15428fc49cb5ec9dad7313112aeeb643
Karen <ipepela1@mail.ru~>
2024-10-29 21:35:53 +0300

    вывод паролей

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿static void DisplayPasswords()
+{
+    Console.WriteLine("Список сохраненных паролей:");
+    foreach (var entry in passwordStore)
+    {
+        Console.WriteLine($"Имя: {entry.Key}, Пароль: {entry.Value}");
+    }
+}
